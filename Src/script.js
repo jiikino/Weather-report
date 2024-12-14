233e02a03f7925046e6af98746bb22eceaea2d1e
JessIkino <jessicaikino@icloud.com>
2024-12-14 10:47:12 -0500

    improved the JS file to dispaly the min and max temp

--- Src/script.js
+++ Src/script.js
@@ -27,6 +27,8 @@ function displayTemp (response){
 
     let windElement = document.querySelector("#current-wind");
     windElement.innerHTML = `${wind}mlh`;
+
+    getForecast(response.data.city);
  }
 
  function formatDate(date) {
@@ -75,26 +77,44 @@ function changeTheme() {
   
   }
 
-  function displayForcast() {
+  function getForecast(city){
+    let apikey = "06cffcec970a84624ftfdo3fb2c867aa";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=metric`;
+    console.log(apiUrl);
+    axios(apiUrl).then(displayForcast);
+  }
+
+  function displayForcast(response) {
+    console.log(response.data);
     let forecastElement = document.querySelector("#forecast");
 
   let days = ["Tue", "Wed","Thu","Fri","Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day){
+  response.data.daily.forEach(function (day, index){
+    if(index < 5) {
      forecastHtml += `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div class="weather-forecast-icon">
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+    </div>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
-        <strong>15º</strong>
+        <strong>${Math.round(day.temperature.maximum)}º</strong>
       </div>
-      <div class="weather-forecast-temperature">9º</div>
+      <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
     </div>
   </div>`;
+    }
   });
   forecastElement.innerHTML = forecastHtml;
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
 }
 
 
@@ -104,7 +124,7 @@ function changeTheme() {
   // Attach the event listener to the button
   let themeButton = document.querySelector("#dark-theme-button");
   themeButton.addEventListener("click", changeTheme);
+  
 
-  displayForcast();
 
   
